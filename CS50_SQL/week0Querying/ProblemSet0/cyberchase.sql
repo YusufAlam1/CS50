68ff7d275ca3d975c8a27c76867dcb10ec0075d4
Yusuf Alam <0yusufalam@gmail.com>
2025-07-03 14:24:05 -0400

    Problem Set 0: Querying

---
+++ CS50_SQL/week0Querying/ProblemSet0/cyberchase.sql
@@ -0,0 +1,13 @@
+-- SELECT "title" FROM "episodes" WHERE season = 1;
+-- SELECT "season", "title" FROM "episodes" WHERE "episode_in_season" = 1;
+-- SELECT "production_code" FROM "episodes" WHERE "title" = 'Hackerized!';
+-- SELECT "title" from "episodes" WHERE "topic" IS NULL;
+-- SELECT "title" FROM "episodes" WHERE "air_date" = '2004-12-31';
+-- SELECT "title" FROM "episodes" WHERE "season" = 6 AND "air_date" < '2008-01-01';
+-- SELECT "title", "topic" FROM "episodes" WHERE "topic" LIKE '%fractions%';
+-- SELECT COUNT(*)FROM "episodes" WHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
+-- SELECT COUNT(*)  FROM "episodes" WHERE "air_date" BETWEEN '2002-01-01' AND '2007-12-31';
+-- SELECT "id", "title", "production_code" FROM "episodes" ORDER BY "production_code" ASC;
+-- SELECT "title" FROM "episodes" WHERE "season" = 5 ORDER BY "title" DESC;
+-- SELECT COUNT(DISTINCT("title")) FROM "episodes";
+-- SELECT "title" FROM "episodes" WHERE LENGTH("title") > 30 OR LENGTH("title") < 10 ORDER BY LENGTH("title") ASC;
